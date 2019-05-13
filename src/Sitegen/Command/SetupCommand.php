456e88275ae80dc98891504e16a24f287a5b9794
Garett Robson <info@garettrobson.co.uk>
2019-05-13 11:50:55 +0100

    Added references and execute method.

--- src/Sitegen/Command/SetupCommand.php
+++ src/Sitegen/Command/SetupCommand.php
@@ -1,6 +1,13 @@
 <?php
 namespace Sitegen\Command;
 
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class SetupCommand extends QuestionCommand
 {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //parent::execute($input, $output);
+    }
 }
